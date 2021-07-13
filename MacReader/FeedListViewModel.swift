40c4321f5537cb32b7129daf581d172cf1a9a34f
Kyosuke Takayama <loiseau@gmail.com>
2021-07-13 14:33:14 +0900

    Add FeedListViewModel

---
+++ MacReader/FeedListViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  FeedListViewModel.swift
+//  MacReader
+//
+//  Created by Kyosuke Takayama on 2021/07/13.
+//
+
+import Foundation
+
+struct FeedItem: Identifiable {
+    let id = UUID()
+    var title: String
+    var contentURL: URL?
+    var imageURL: String?
+}
+
+final class FeedListViewModel: ObservableObject {
+    @Published var items: [FeedItem]
+
+    init(items: [FeedItem]) {
+        self.items = items
+    }
+}
