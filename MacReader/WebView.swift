6782db3e6ba16c949a21741f0dc8391be4642128
Kyosuke Takayama <loiseau@gmail.com>
2021-07-13 12:11:22 +0900

    Add WebView

---
+++ MacReader/WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  MacReader
+//
+//  Created by Kyosuke Takayama on 2021/07/13.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView : UIViewRepresentable {
+    var url: URL
+
+    func makeUIView(context: Context) -> WKWebView  {
+        return WKWebView(frame: .zero)
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let req = URLRequest(url: url)
+        uiView.load(req)
+    }
+}
