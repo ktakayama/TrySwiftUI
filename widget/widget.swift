911e9998e2adc00aec15007dc3d70a9105a0f5a2
Kyosuke Takayama <loiseau@gmail.com>
2021-07-14 19:04:11 +0900

    Add WidgetView

--- widget/widget.swift
+++ widget/widget.swift
@@ -9,40 +9,23 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+    func placeholder(in context: Context) -> WidgetEntry {
+        WidgetEntry(items: [])
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(in context: Context, completion: @escaping (WidgetEntry) -> ()) {
+        let entry = WidgetEntry(items: [])
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+    func getTimeline(in context: Context, completion: @escaping (Timeline<WidgetEntry>) -> ()) {
+        var entries: [WidgetEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
+        let entry = WidgetEntry(items: [])
+        entries.append(entry)
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-}
-
-struct widgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
     }
 }
 
@@ -52,16 +35,9 @@ struct widget: Widget {
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            widgetEntryView(entry: entry)
+            widgetView(entry: entry)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
-    }
-}
-
-struct widget_Previews: PreviewProvider {
-    static var previews: some View {
-        widgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
---
+++ widget/widgetView.swift
@@ -0,0 +1,68 @@
+//
+//  widgetView.swift
+//  widgetExtension
+//
+//  Created by Kyosuke Takayama on 2021/07/14.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct widgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        let items = [
+            WidgetEntryItem(title: "すごい安いディスプレイがやばすぎて祭りに"),
+            WidgetEntryItem(title: "すごい安いディスプレイがやばすぎて祭りに"),
+            WidgetEntryItem(title: "Linus が")
+        ]
+        widgetView(entry: WidgetEntry(items: items))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+    }
+}
+
+struct widgetView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        HStack {
+            VStack {
+                ForEach(entry.items) { item in
+                    widgetItemView(item: item)
+                }
+            }
+            Spacer()
+        }
+        .padding(14)
+        .background(Color(UIColor.systemBackground))
+    }
+}
+
+struct widgetItemView: View {
+    let item: WidgetEntryItem
+
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 2)
+                .foregroundColor(.gray)
+                .frame(width: 4, height: 30, alignment: .center)
+            VStack(alignment: .leading) {
+                Text(item.title)
+                    .font(.subheadline)
+                Text(item.date, style: .time)
+                    .font(.caption2)
+            }
+            Spacer()
+        }
+    }
+}
+
+struct WidgetEntry: TimelineEntry {
+    let date = Date()
+    let items: [WidgetEntryItem]
+}
+
+struct WidgetEntryItem: Identifiable {
+    let id = UUID()
+    let date = Date()
+    let title: String
+}
