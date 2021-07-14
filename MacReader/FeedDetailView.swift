858e7090fe54be9803dacd74cb73ac67eff77a69
Kyosuke Takayama <loiseau@gmail.com>
2021-07-14 19:56:54 +0900

    Add sample data

--- MacReader/FeedDetailView.swift
+++ MacReader/FeedDetailView.swift
@@ -10,8 +10,38 @@ import SwiftUI
 struct FeedDetailView: View {
     var title: String
     var url: URL?
+    @State private var username: String = ""
 
     var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("おなまえ")
+                    .font(.system(size: 16))
+                TextField("閑歳 孝子", text: $username)
+                    .border(Color(UIColor.separator))
+                Text("ご住所")
+                    .font(.system(size: 16))
+                TextField("千代田区1−1−1", text: $username)
+                    .border(Color(UIColor.separator))
+                Spacer()
+            }
+            .navigationBarTitle("Navigation title")
+            .padding(16)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    var body_: some View {
         NavigationView {
             VStack {
                 WebView(url: url!)
